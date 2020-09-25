718b69ad921bdb6a104a9222da86ca1358f31e9f
Jake Ruesink <jaruesink@gmail.com>
2020-09-25 00:40:12 -0500

    Updating types for Form to try out Generic types

--- src/lib/form/Form.tsx
+++ src/lib/form/Form.tsx
@@ -10,16 +10,13 @@ interface UnsavedChangesConfig {
   modalProps?: ReactModal.Props;
 }
 
-export function Form<T>({
-  className,
-  children,
-  unsavedChangesConfig = {},
-  ...rest
-}: FormikConfig<T> & {
-  className?: string;
-  unsavedChangesConfig?: UnsavedChangesConfig;
-  children: (formikProps: FormikProps<T>) => React.ReactNode;
-}) {
+type FormProps<T> = FormikConfig<T> &
+  React.FC<{ children: (formikProps: FormikProps<T>) => React.ReactNode }> & {
+    className?: string;
+    unsavedChangesConfig?: UnsavedChangesConfig;
+  };
+
+export function Form<T extends Object>({ className, children, unsavedChangesConfig = {}, ...rest }: FormProps<T>) {
   const formRef = useRef<FormikProps<T>>(null);
   const [activeModal, setActiveModal] = useState<'none' | 'unsavedChangesModal'>('none');
   const [shouldCheckForUnsavedChanges, setShouldCheckForUnsavedChanges] = useState<boolean>(true);
