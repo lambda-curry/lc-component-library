ea86068cdabef80576bd168ae6d3ca2bdf18d8a4
Jake Ruesink <jaruesink@gmail.com>
2020-09-29 11:28:06 -0500

    Updating InputBase types

--- src/lib/inputs/InputBase.tsx
+++ src/lib/inputs/InputBase.tsx
@@ -8,37 +8,37 @@ import './input.scss';
 import { InputAdornment } from '@material-ui/core';
 
 export type InputProps = TextFieldProps & {
-    name: string;
-    prefix?: JSX.Element;
-    suffix?: JSX.Element;
-    formikProps: FormikProps<{}>;
+  name: string;
+  prefix?: JSX.Element;
+  suffix?: JSX.Element;
+  formikProps: FormikProps<any>;
 };
 
 export const InputBase = ({ name, className, formikProps, prefix, suffix, ...props }: InputProps) => {
-    const fieldError =
-        formikProps.errors && name && _get(formikProps.touched, name) ? _get(formikProps.errors, name) : '';
+  const fieldError =
+    formikProps.errors && name && _get(formikProps.touched, name) ? _get(formikProps.errors, name) : '';
 
-    const fieldValue = _get(formikProps.values, name);
+  const fieldValue = _get(formikProps.values, name);
 
-    const inputProps = {
-        startAdornment: prefix ? <InputAdornment position="start">{prefix}</InputAdornment> : false,
-        endAdornment: suffix ? <InputAdornment position="end">{suffix}</InputAdornment> : false
-    };
+  const inputProps = {
+    startAdornment: prefix ? <InputAdornment position="start">{prefix}</InputAdornment> : false,
+    endAdornment: suffix ? <InputAdornment position="end">{suffix}</InputAdornment> : false
+  };
 
-    return (
-        <TextField
-            name={name}
-            error={!!fieldError}
-            helperText={fieldError}
-            size="small"
-            className={classNames(className, 'input')}
-            // Note: `as any` needed to help with expected types for the variant prop from Material UI
-            variant={'outlined' as any}
-            InputProps={inputProps}
-            value={fieldValue}
-            onChange={formikProps.handleChange}
-            onBlur={formikProps.handleBlur}
-            {...props}
-        />
-    );
+  return (
+    <TextField
+      name={name}
+      error={!!fieldError}
+      helperText={fieldError}
+      size="small"
+      className={classNames(className, 'input')}
+      // Note: `as any` needed to help with expected types for the variant prop from Material UI
+      variant={'outlined' as any}
+      InputProps={inputProps}
+      value={fieldValue}
+      onChange={formikProps.handleChange}
+      onBlur={formikProps.handleBlur}
+      {...props}
+    />
+  );
 };
