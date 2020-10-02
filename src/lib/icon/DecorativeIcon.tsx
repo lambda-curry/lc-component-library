74ff4a426e9712ea78d978d8ba0c7de4cbb738a5
Jake Ruesink <jaruesink@gmail.com>
2020-10-01 23:26:38 -0500

    Icon centering and input height to match button height at 48px

--- src/lib/icon/DecorativeIcon.tsx
+++ src/lib/icon/DecorativeIcon.tsx
@@ -3,20 +3,9 @@ import classNames from 'classnames';
 import { Icon, DefaultIconNames } from './Icon';
 import { getCssVar } from '../util/colors';
 
-export type DecorativeIconBackgroundColors =
-  | 'primary_color'
-  | 'secondary_color'
-  | 'black'
-  | 'red'
-  | 'green'
-  | 'blue'
-  | 'light-blue'
-  | 'app-green'
-  | 'app-red';
-
 export const DecorativeIcon: React.FC<{
   className?: string;
-  color: DecorativeIconBackgroundColors | string;
+  color: string;
   name: DefaultIconNames | string;
 }> = ({ className, color, ...props }) => {
   // const colorIsVariable = [].includes(color);
