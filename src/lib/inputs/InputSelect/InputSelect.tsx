02230b02d56ade36d7ddfc96e8a8df5e835f0a7d
Jared Hill <jaredbhill@gmail.com>
2020-10-21 10:07:45 -0500

    adding more Mui AutoComplete props to InputSelect and updating InputSelect icon styles

--- src/lib/inputs/InputSelect/InputSelect.tsx
+++ src/lib/inputs/InputSelect/InputSelect.tsx
@@ -13,32 +13,47 @@ type InputSelectProps = InputProps & {
   options: any[];
   optionLabelKey?: string;
   multiple?: boolean;
+  disableClearable?: boolean;
+  freeSolo?: boolean;
+  openOnFocus?: boolean;
 };
 
 export const InputSelect: FunctionComponent<InputSelectProps> = ({
   options,
   optionLabelKey = 'label',
   multiple,
+  disableClearable,
+  freeSolo,
+  openOnFocus = true,
   name,
   className,
   formikProps,
   ...props
 }) => {
-  const autocompleteConfig: AutocompleteProps<any, boolean, boolean, boolean> = {
-    options,
-    multiple,
-    value: formikProps.values[name],
-    openOnFocus: true,
-    closeIcon: <Icon className="input-select-icon-close" name="close" />,
-    popupIcon: <Icon className="input-select-icon-popup" name="chevronDown" />,
-    ChipProps: { deleteIcon: <Icon name="close" /> },
-    renderInput: inputProps => (
-      <InputText name={name} {...inputProps} {...props} formikProps={{ ...formikProps, handleChange: () => {} }} />
-    ),
-    PaperComponent: props => <Paper className="input-select-paper" {...props} />,
-    getOptionLabel: (option: { [key: string]: any }) => option[optionLabelKey] || '',
-    getOptionSelected: (option: any, value: any) => isEqual(option, value),
-    onChange: (event: object, value: any) => formikProps.setFieldValue(name, value)
+  const autocompleteConfig = {
+      options,
+      multiple,
+      disableClearable,
+      freeSolo,
+      openOnFocus,
+      value: formikProps?.values[name],
+      closeIcon: <Icon className="input-select-icon-close" name="close" />,
+      popupIcon: <Icon className="input-select-icon-popup" name="chevronDown" />,
+      ChipProps: { deleteIcon: <Icon name="close" /> },
+      renderInput: (inputProps: any) => (
+          <InputText
+              name={name}
+              {...inputProps}
+              {...props}
+              formikProps={{ ...formikProps, handleChange: () => null }}
+          />
+      ),
+      PaperComponent: (paperComponentProps: any) => (
+          <Paper className="input-select-paper" {...paperComponentProps} />
+      ),
+      getOptionLabel: (option: { [key: string]: any }) => option[optionLabelKey] || '',
+      getOptionSelected: (option: any, value: any) => isEqual(option, value),
+      onChange: (event: object, value: any) => formikProps?.setFieldValue(name, value),
   };
 
   return <Autocomplete className={classNames('input-select', className)} {...autocompleteConfig} />;
