e3fcba7315ce64c07d1eaf9e28f63d00bb679cf9
Carlo Piantini <carlo.piantini@gmail.com>
2020-11-04 18:35:54 -0500

    Customizing component styles to match the rest of the component library

--- src/lib/tabs/Tabs.tsx
+++ src/lib/tabs/Tabs.tsx
@@ -27,8 +27,31 @@ function a11yProps(index: any) {
 }
 
 const useStyles = makeStyles((theme: Theme) => ({
+  appBar: {
+    backgroundColor: 'rgba(0,0,0,0)',
+    boxShadow: 'none'
+  },
   root: {
-    flexGrow: 1
+    borderRadius: '4px',
+    boxShadow: '0px 2px 4px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12)',
+    flexGrow: 1,
+    fontFamily: 'lc-gilroy'
+  },
+  tab: {
+    borderBottom: '2px solid gray',
+    color: 'black',
+    flex: 1,
+    fontFamily: 'inherit',
+    maxWidth: 'none',
+    '&:focus': {
+      outline: 'none'
+    },
+    '&.Mui-selected': {
+      fontWeight: '700'
+    }
+  },
+  tabPanel: {
+    padding: '16px'
   }
 }));
 
@@ -45,16 +68,16 @@ export const Tabs: React.FC<{ ariaLabel?: string; tabs: { label: string; render:
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar className={classes.appBar} position="static">
         <MuiTabs value={value} onChange={handleChange} aria-label={ariaLabel}>
           {tabs.map((tab, index) => (
-            <Tab label={tab.label} {...a11yProps(index)} />
+            <Tab className={classes.tab} label={tab.label} {...a11yProps(index)} />
           ))}
         </MuiTabs>
       </AppBar>
 
       {tabs.map((tab, index) => (
-        <TabPanel value={value} index={index}>
+        <TabPanel className={classes.tabPanel} value={value} index={index}>
           {tab.render}
         </TabPanel>
       ))}
