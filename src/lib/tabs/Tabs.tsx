2debec83c706b17f282787cf81cb23217c5bed47
Jake Ruesink <jaruesink@gmail.com>
2020-11-05 17:59:45 -0600

    Add textTransform as none to the Tabs component

--- src/lib/tabs/Tabs.tsx
+++ src/lib/tabs/Tabs.tsx
@@ -39,6 +39,7 @@ const useStyles = makeStyles((theme: Theme) => ({
     fontFamily: 'inherit'
   },
   tab: {
+    textTransform: 'none',
     borderBottom: '2px solid var(--primary-light-color, #5e9bd5)',
     color: 'black',
     fontFamily: 'inherit',
