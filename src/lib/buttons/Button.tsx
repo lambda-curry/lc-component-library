4a7ef4ac318eebe585e0581d2bd5ded6d2354b81
Jake Ruesink <jaruesink@gmail.com>
2020-11-06 02:05:23 -0600

    Updating Input Select component to take an optionValueKey and fixing minor issues with the Button components

--- src/lib/buttons/Button.tsx
+++ src/lib/buttons/Button.tsx
@@ -1,21 +1,15 @@
-import React, { ButtonHTMLAttributes, Reducer, useEffect, useReducer, useRef } from 'react';
+import React, { ButtonHTMLAttributes, forwardRef, Reducer, useEffect, useReducer, useRef } from 'react';
 import classNames from 'classnames';
 import './buttons.scss';
 import { buttonReducer, ButtonReducerState, ButtonReducerAction } from './Button.helpers';
 
 export type ButtonProps = ButtonHTMLAttributes<any> & {
-  ref?: React.MutableRefObject<any>;
   as?: (props: ButtonHTMLAttributes<{ children: React.ReactNode }>) => JSX.Element;
   type?: 'button' | 'submit' | 'reset' | undefined;
 };
 
-export const Button: React.FC<ButtonProps> = ({
-  className,
-  as = props => <button {...props} />,
-  type = 'button',
-  ...rest
-}) => {
-  const ref = useRef<HTMLElement>(null);
+export const Button: React.FC<ButtonProps> = ({ className, as, type = 'button', ...rest }) => {
+  const ref = useRef<HTMLButtonElement>(null);
   const [{ leftIcon, rightIcon }, dispatch] = useReducer<Reducer<ButtonReducerState, ButtonReducerAction>>(
     buttonReducer,
     {
@@ -31,11 +25,13 @@ export const Button: React.FC<ButtonProps> = ({
       dispatch({ name: 'setRightIcon', payload: true });
   }, [ref]);
 
-  return <>{as({ className: classNames('lc-button', { leftIcon, rightIcon }, className), type, ref, ...rest })}</>;
+  const ButtonComponent = as ? as : (props: ButtonProps) => <button ref={ref} {...props} />;
+  const buttonProps = { className: classNames('lc-button', { leftIcon, rightIcon }, className), type, ...rest };
+  return <ButtonComponent {...buttonProps} />;
 };
 
 // Now we can simply pass in any element to use as a button
-export const AnchorButton = (props: ButtonProps) => <Button as={renderProps => <a {...renderProps} {...props} />} />;
+export const AnchorButton = (props: ButtonProps) => <Button {...props} as={renderProps => <a {...renderProps} />} />;
 
 function containsSVG(el: Element | null) {
   return el?.querySelector && !!el?.querySelector('svg');
--- src/lib/inputs/InputSelect/InputSelect.tsx
+++ src/lib/inputs/InputSelect/InputSelect.tsx
@@ -17,8 +17,10 @@ export type AutoCompleteChange = (
 ) => void;
 
 export type InputSelectProps = Omit<InputProps, 'onChange'> & {
+  value: { [x: string]: any };
   options: any[];
   optionLabelKey?: string;
+  optionValueKey?: string;
   autocompleteConfig?: Partial<AutocompleteProps<any, boolean, boolean, boolean>>;
   onChange?: AutoCompleteChange;
 };
@@ -26,6 +28,7 @@ export type InputSelectProps = Omit<InputProps, 'onChange'> & {
 export const InputSelect: React.FC<InputSelectProps> = ({
   options,
   optionLabelKey = 'label',
+  optionValueKey,
   name,
   className,
   autocompleteConfig,
@@ -35,7 +38,8 @@ export const InputSelect: React.FC<InputSelectProps> = ({
   const initialValue = isNullOrUndefined(props.formikProps?.values[name])
     ? props.value || null
     : props.formikProps?.values[name];
-  const [inputValue, setInputValue] = useState(initialValue);
+  const [inputTextValue, setInputTextValue] = useState(initialValue);
+  const [fieldValue, setFieldValue] = useState(optionValueKey ? initialValue[optionValueKey] : initialValue);
 
   // Note: We want to remove the change event from the rendered component so it can be handled by the autocomplete
   if (props.formikProps) {
@@ -48,9 +52,11 @@ export const InputSelect: React.FC<InputSelectProps> = ({
     reason: AutocompleteChangeReason,
     details?: AutocompleteChangeDetails<any> | undefined
   ) => void = (event, value, reason, details) => {
-    setInputValue(value);
-    if (props.formikProps) props.formikProps.setFieldValue(name, value);
-    if (typeof onChange === 'function') onChange(event, value, reason, details);
+    const fieldValue = optionValueKey ? value[optionValueKey] : value;
+    setInputTextValue(value);
+    setFieldValue(fieldValue);
+    if (props.formikProps) props.formikProps.setFieldValue(name, fieldValue);
+    if (typeof onChange === 'function') onChange(event, fieldValue, reason, details);
   };
 
   const autocompleteProps: AutocompleteProps<any, boolean, boolean, boolean> = {
@@ -58,12 +64,20 @@ export const InputSelect: React.FC<InputSelectProps> = ({
     multiple: false,
     // Note: Value initialized as null instead of undefined to help with uncontrolled component warning
     // https://github.com/mui-org/material-ui/issues/18173#issuecomment-552420187
-    value: inputValue,
+    value: inputTextValue,
     openOnFocus: true,
     closeIcon: <Icon className="input-select-icon-close" name="close" />,
     popupIcon: <Icon className="input-select-icon-popup" name="chevronDown" />,
     ChipProps: { deleteIcon: <Icon name="close" /> },
-    renderInput: inputProps => <InputText name={name} {...inputProps} {...props} />,
+    renderInput:
+      !optionValueKey || props.formikProps
+        ? inputProps => <InputText name={name} {...inputProps} {...props} />
+        : inputProps => (
+            <>
+              <input name={name} type="hidden" value={props.value[optionValueKey]} />
+              <InputText name={`_${name}`} {...inputProps} {...props} />
+            </>
+          ),
     PaperComponent: props => <Paper className="input-select-paper" {...props} />,
     getOptionSelected: (option: any, value: any) => isEqual(option, value),
     getOptionLabel: (option: { [key: string]: any }) => get(option, optionLabelKey) || '',
