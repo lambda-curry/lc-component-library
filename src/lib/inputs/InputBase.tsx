5b5c54840695939693750221154f52f784796e14
Jake Ruesink <jaruesink@gmail.com>
2020-11-07 15:32:23 -0600

    Input Base - add way to pass in props to the input directly

--- src/lib/inputs/InputBase.tsx
+++ src/lib/inputs/InputBase.tsx
@@ -5,7 +5,7 @@ import classNames from 'classnames';
 import { get as _get } from 'lodash';
 
 import './input.scss';
-import { InputAdornment } from '@material-ui/core';
+import { InputAdornment, OutlinedInputProps } from '@material-ui/core';
 
 type LabelPlacements = 'inset' | 'above';
 
@@ -35,9 +35,10 @@ export const InputBase: React.FC<InputProps> = ({
 
   const fieldValue = formikProps ? _get(formikProps?.values, name) : props.value;
 
-  const inputProps = {
+  const inputProps: any = {
     startAdornment: prefix ? <InputAdornment position="start">{prefix}</InputAdornment> : false,
-    endAdornment: suffix ? <InputAdornment position="end">{suffix}</InputAdornment> : false
+    endAdornment: suffix ? <InputAdornment position="end">{suffix}</InputAdornment> : false,
+    ...props.inputProps
   };
 
   const handleChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
@@ -60,10 +61,10 @@ export const InputBase: React.FC<InputProps> = ({
       <TextField
         name={name}
         id={id || name}
-        InputProps={inputProps}
         label={labelPlacement === 'inset' ? label : false}
         size="small"
         {...props}
+        InputProps={inputProps}
         error={!!fieldError}
         helperText={fieldError || props.helperText}
         className={classNames(className, { 'lc-input-label-above': labelPlacement === 'above' }, 'lc-input')}
