ee3b6324edb611370db0452cf2f72b009b7913f4
Jared Hill <jaredbhill@gmail.com>
2020-11-28 18:32:15 -0600

    add InputTextarea component

--- src/lib/inputs/index.ts
+++ src/lib/inputs/index.ts
@@ -2,6 +2,7 @@ export { InputCheckbox } from './InputCheckbox/InputCheckbox';
 export { InputSelect } from './InputSelect/InputSelect';
 export { InputSwitch } from './InputSwitch/InputSwitch';
 export { InputText } from './InputText/InputText';
+export { InputTextarea } from './InputTextarea/InputTextarea';
 export { InputDate } from './InputDate/InputDate';
 export { InputPhone } from './InputPhone/InputPhone';
 export { InputSearch } from './InputSearch/InputSearch';
