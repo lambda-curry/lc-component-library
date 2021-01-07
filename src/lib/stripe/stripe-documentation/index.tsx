38ebdb7601b5fbad82454dfb1370946f5903fd89
Jake Ruesink <jaruesink@gmail.com>
2021-01-06 19:38:59 -0600

    Adding StripeFormWrapperContent to the StripeFormWrapper component

--- src/lib/stripe/stripe-documentation/index.tsx
+++ src/lib/stripe/stripe-documentation/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Form, StripeCardInput, StripeFormWrapper } from '../..';
+import { StripeCardInput, StripeFormWrapper } from '../..';
 
 export const StripeExample1 = () => (
-  <StripeFormWrapper STRIPE_KEY={process.env.STORYBOOK_STRIPE_KEY}>
-    <Form initialValues={{ 'stripe-input': undefined }} onSubmit={() => {}}>
-      {formikProps => (
-        <div className="stripe-story">
-          <p>Values: {JSON.stringify(formikProps.values)}</p>
-          <StripeCardInput label="Stripe Input" name="stripe-input" />
-        </div>
-      )}
-    </Form>
+  <StripeFormWrapper
+    initialValues={{ 'stripe-input': undefined }}
+    STRIPE_KEY={process.env.STORYBOOK_STRIPE_KEY}
+    onSubmit={() => {}}
+  >
+    {formikProps => (
+      <div className="stripe-story">
+        <p>Values: {JSON.stringify(formikProps.values)}</p>
+        <StripeCardInput label="Stripe Input" name="stripe-input" />
+      </div>
+    )}
   </StripeFormWrapper>
 );
