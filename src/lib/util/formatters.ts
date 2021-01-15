f64aa3b55764edb6f0e966be8136330fa010eaaa
Jared Hill <jaredbhill@gmail.com>
2021-01-15 09:30:57 -0600

    doing some cleanup

--- src/lib/util/formatters.ts
+++ src/lib/util/formatters.ts
@@ -1,11 +1,8 @@
 export const stripTags = (value: string) => value.replace(/(<([^>]+)>)/gi, '');
 
-export const arrayToListString = (arr: string[], finalSeparator: string) => {
-  return arr.reduce((acc, curr, index) => {
+export const arrayToListString = (arr: string[], finalSeparator: string) =>
+  arr.reduce((acc, curr, index) => {
     if (index !== 0 && index === arr.length - 1) return `${acc} ${finalSeparator} ${curr}`;
-
     if (index > 0) return `${acc}, ${curr}`;
-
     return curr;
   }, '');
-};
