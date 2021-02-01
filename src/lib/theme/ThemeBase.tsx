442dee16fcd171abe642060c81f11f5a81e238a7
Jared Hill <jaredbhill@gmail.com>
2021-02-01 11:35:25 -0600

    chore: migrate code base to tsdx

--- src/lib/theme/ThemeBase.tsx
+++ src/lib/theme/ThemeBase.tsx
@@ -1,12 +1,14 @@
 import React, { FC, ReactNode } from 'react';
+import classNames from 'classnames';
 
 import '../../styles/index.scss';
 
 export interface ThemeBaseProps {
   as?: keyof JSX.IntrinsicElements;
   children: ReactNode;
+  className?: string;
 }
 
-export const ThemeBase: FC<ThemeBaseProps> = ({ as: T = 'div', ...props }) => (
-  <T className="lc-theme-base" {...props} />
+export const ThemeBase: FC<ThemeBaseProps> = ({ as: T = 'div', className, ...props }) => (
+  <T className={classNames('lc-theme-base', className)} {...props} />
 );
