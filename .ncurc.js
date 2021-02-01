77381b7696e435da953f4db3fb4f4ccd06a4f775
Jared Hill <jaredbhill@gmail.com>
2021-02-01 12:40:26 -0600

    upgrading some node packages and adding .ncurc file to reject updating certain packages

---
+++ .ncurc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  reject: [
+    // Let's make sure we are deliberate about when we update React.
+    'react',
+    'react-dom',
+
+    // Not all our packages currently support PostCSS v8, so we need to wait for them to add support.
+    'autoprefixer',
+    'postcss',
+    'postcss-cli',
+    'postcss-scss',
+    'rollup-plugin-postcss',
+  ]
+}
