6bcaef716f97f80e0d89812eb5d4277a5047da09
Jared Hill <jaredbhill@gmail.com>
2021-02-08 14:54:55 -0600

    fixing tailwind black and white colors to be hex format

--- tailwind/colors.js
+++ tailwind/colors.js
@@ -10,8 +10,8 @@ const colorWithOpactiy = (color) => ({ opacityVariable, opacityValue }) => {
 const baseColors = {
   transparent: 'transparent',
   current: 'currentColor',
-  black: colorWithOpactiy('black'),
-  white: colorWithOpactiy('white'),
+  black: colorWithOpactiy('#000'),
+  white: colorWithOpactiy('#fff'),
 };
 
 const themeColorsConfig = {
