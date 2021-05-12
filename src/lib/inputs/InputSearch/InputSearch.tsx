9764154c2ea5b7445d6bac4f95e88a12a934c33f
Jake Ruesink <jaruesink@gmail.com>
2021-05-12 15:54:11 -0500

    fix(input-search): allowing falsey search values on initial search

--- src/lib/inputs/InputSearch/InputSearch.tsx
+++ src/lib/inputs/InputSearch/InputSearch.tsx
@@ -85,9 +85,17 @@ export const InputSearch: FC<InputSearchProps> = ({
 
   const searchTerm = useDebounce(state.inputSearchValue, config.debounceTime || 200);
   const search = async () => {
+    console.log('>>> 1', typeof searchTerm, config.ignoreFalseyInputValues);
     // If the input value equals, we probably do not need to run another search - Jake 05/06/2021
-    if (searchTerm === getValueLabel(selectedValue)) return;
-    if (!config.initialSearchValue && config.ignoreFalseyInputValues && !state.inputSearchValue) return;
+    if (!!searchTerm && searchTerm === getValueLabel(selectedValue)) return;
+    if (
+      config.ignoreFalseyInputValues &&
+      (config.initialSearchValue === undefined || config.initialSearchValue === null) &&
+      (state.inputSearchValue === undefined || state.inputSearchValue === null)
+    )
+      return;
+
+    console.log('>>> 2', searchTerm);
     const [base, params] = url.split('?');
     const searchParams = new URLSearchParams(params);
     if (searchParam) searchParams.set(searchParam, searchTerm);
