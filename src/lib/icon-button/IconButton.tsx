c7f8ce2d1baab41d214790bc0225f2bddf7b9d2f
Jake Ruesink <jaruesink@gmail.com>
2021-06-25 11:17:28 -0500

    feat: prototype of esbuild setup for component library - fixing button styled import in icon button

--- src/lib/icon-button/IconButton.tsx
+++ src/lib/icon-button/IconButton.tsx
@@ -1,8 +1,8 @@
 import React, { FC } from 'react';
 import classNames from 'classnames';
 import { Icon, IconNames } from '../icon/Icon';
-import { ButtonStyled, ButtonProps } from '../..';
-
+import { ButtonProps } from '../buttons/ButtonBase';
+import { ButtonStyled } from '../buttons/ButtonStyled';
 import './icon-button.css';
 
 export interface IconButtonProps extends ButtonProps {
