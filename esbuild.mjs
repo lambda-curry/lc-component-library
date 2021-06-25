a15e7b1cb2051a7d3c3be1a09e41ccae14aa8dcb
Jake Ruesink <jaruesink@gmail.com>
2021-06-25 12:06:53 -0500

    feat: prototype of esbuild setup for component library - fixing blob for building all files

--- esbuild.mjs
+++ esbuild.mjs
@@ -16,7 +16,12 @@ import cssnano from 'cssnano';
 import purgecss from '@fullhuman/postcss-purgecss';
 
 
-const entryPoints = ['./src/index.ts', ...glob.sync('./src/**/!(*documentation)/**/*[!*.d].{ts,tsx,css}')];
+const entryPoints = ['./src/index.ts', ...glob.sync('./src/**/!(*documentation|.d.ts)/*+(!d.ts|.*[!d].ts|.tsx|.css)')];
+
+// Note: this will currently not match if the following component since the second section ends in d
+// but this seems trivial enough. Who knew blobs would be so confusing. :-P - Jake 06/25/2021
+// ./src/lib/buttons/Button.adsfasdfd.ts
+
 
 const options = {
   entryPoints,
