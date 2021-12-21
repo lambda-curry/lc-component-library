e0ef14a962ea025a60963affdbe3700a9a7a486a
Jared Hill <jaredbhill@gmail.com>
2021-12-21 12:55:25 -0600

    fix: add InputTextProps type and export InputBase from inputs (#95)

--- packages/ui-components/src/lib/inputs/InputGroup/input-group-documentation/input-group.examples.tsx
+++ packages/ui-components/src/lib/inputs/InputGroup/input-group-documentation/input-group.examples.tsx
@@ -6,20 +6,18 @@ import { InputText } from '../../InputText/InputText';
 import { InputGroup } from '../InputGroup';
 
 export const InputGroupExample: FC<any> = () => (
-  <>
-    <InputGroup>
-      <InputText label="Sample Input" name="sampleInput" />
-      <InputSelect
-        label="Sample Select"
-        name="sampleSelect"
-        options={[
-          { label: 'Option 1', value: '1' },
-          { label: 'Option 2', value: '2' },
-          { label: 'Option 3', value: '3' }
-        ]}
-      />
-      <Button>Clear</Button>
-      <ButtonPrimary>Save</ButtonPrimary>
-    </InputGroup>
-  </>
+  <InputGroup>
+    <InputText label="Sample Input" name="sampleInput" />
+    <InputSelect
+      label="Sample Select"
+      name="sampleSelect"
+      options={[
+        { label: 'Option 1', value: '1' },
+        { label: 'Option 2', value: '2' },
+        { label: 'Option 3', value: '3' }
+      ]}
+    />
+    <Button>Clear</Button>
+    <ButtonPrimary>Save</ButtonPrimary>
+  </InputGroup>
 );
