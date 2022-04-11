435e2b8b4e48028341be06a29142aa28cbdfeaf8
Jared Hill <jaredbhill@gmail.com>
2022-04-11 11:14:53 -0500

    enhancement(icon-button): allow passing in custom icon component or iconProps

--- packages/ui-components/src/lib/icon-button/IconButton.tsx
+++ packages/ui-components/src/lib/icon-button/IconButton.tsx
@@ -1,20 +1,21 @@
-import React, { FC, forwardRef } from 'react';
+import React, { FC, forwardRef, ReactNode } from 'react';
 import classNames from 'classnames';
-import { Icon, IconNames } from '../icon/Icon';
+import { Icon, IconNames, IconProps } from '../icon/Icon';
 import { ButtonProps } from '../buttons/ButtonBase';
 import { ButtonStyled } from '../buttons/ButtonStyled';
 import './icon-button.css';
 
 export interface IconButtonProps extends ButtonProps {
-  icon: IconNames;
+  icon: IconNames | ReactNode;
+  iconProps?: IconProps;
 }
 
-export const IconButton: FC<IconButtonProps> = forwardRef(({ className, icon, ...props }, ref) => (
+export const IconButton: FC<IconButtonProps> = forwardRef(({ className, icon, iconProps, ...props }, ref) => (
   <ButtonStyled
     {...props}
     className={classNames([`lc-icon-button`, `focus-visible:lc-ring-gray-lighter`], className)}
     ref={ref}
   >
-    <Icon name={icon} />
+    {typeof icon === 'string' ? <Icon name={icon} {...iconProps} /> : icon}
   </ButtonStyled>
 ));
