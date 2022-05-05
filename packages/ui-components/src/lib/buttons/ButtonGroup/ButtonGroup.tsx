4b6c0a143294b8fe2feb9fc9a5905316ae263d20
Jared Hill <jaredbhill@gmail.com>
2022-05-05 11:46:10 -0500

    chore: add support for React 18

--- packages/ui-components/src/lib/buttons/ButtonGroup/ButtonGroup.tsx
+++ packages/ui-components/src/lib/buttons/ButtonGroup/ButtonGroup.tsx
@@ -1,8 +1,9 @@
-import React, { FC } from 'react';
+import React, { FC, PropsWithChildren } from 'react';
 import classNames from 'classnames';
 import './button-group.css';
-import { PropsWithChildren } from 'react';
 
-export const ButtonGroup: FC<PropsWithChildren<{ className?: string }>> = ({ className, ...props }) => (
-  <div {...props} className={classNames('lc-button-group', className)} />
+export type ButtonGroupProps = PropsWithChildren<{ className?: string }>;
+
+export const ButtonGroup: FC<ButtonGroupProps> = ({ className, ...props }) => (
+  <div className={classNames('lc-button-group', className)} {...props} />
 );
