993ea17cfbc8d1d493fdcafe86ffc4807f16b4e5
Jared Hill <jaredbhill@gmail.com>
2022-05-05 13:50:02 -0500

    chore: add support for React 18

--- packages/ui-components/src/lib/hooks/useFormContext.ts
+++ packages/ui-components/src/lib/hooks/useFormContext.ts
@@ -2,5 +2,5 @@ import { useContext } from 'react';
 import { FormikContext, FormikContextType } from 'formik';
 
 // Note: Using context this way instead of useFormikContext suppresses the error if it is not within Formik
-// TODO: Need to fix formik types at some point, but coercing `any` for now...
-export const useFormContext = <T>() => useContext<FormikContextType<T>>(FormikContext as any); // Using context this way instead of useFormikContext suppresses the error if it is not within Formik
+// @ts-ignore
+export const useFormContext = <T>() => useContext<FormikContextType<T>>(FormikContext); // Using context this way instead of useFormikContext suppresses the error if it is not within Formik
