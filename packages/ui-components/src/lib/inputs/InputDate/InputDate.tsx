723e579a0b84ff72370499d3e59c6e39019eb6d3
Jake Ruesink <jaruesink@gmail.com>
2022-12-03 23:27:57 -0600

    feat(input-date): fix ability to type into date input

--- packages/ui-components/src/lib/inputs/InputDate/InputDate.tsx
+++ packages/ui-components/src/lib/inputs/InputDate/InputDate.tsx
@@ -18,13 +18,6 @@ export type InputDateProps = Omit<InputProps, 'onChange'> & {
   datePickerProps?: Partial<DatePickerProps<DateTime, DateTime>>;
 };
 
-const fromDateTime = (dt: DateTime | null, format?: string): Date | string | null => {
-  console.log('>>>> onChange', dt);
-  if (!dt) return null;
-  if (format) return dt.toFormat(format);
-  return dt.toJSDate();
-};
-
 export const InputDate: FC<InputDateProps> = ({
   label = 'Select Date',
   value,
@@ -39,27 +32,29 @@ export const InputDate: FC<InputDateProps> = ({
 }) => {
   const formContext = useFormContext();
   if (!formikProps && formContext) formikProps = formContext;
-
   const fieldValue = formikProps ? _get(formikProps?.values, props.name, '') : value;
-  console.log('>>> fieldValue', fieldValue);
-  // console.log(`>>> values ${props.name}:`, JSON.stringify(formikProps?.values));
-  // console.log(`>>> errors ${props.name}:`, JSON.stringify(formikProps?.errors));
+  const [inputValue, setInputValue] = React.useState<DateTime | null>(fieldValue);
 
   return (
     <LocalizationProvider dateAdapter={AdapterLuxon}>
       <DatePicker
         {...datePickerProps}
         label={label}
-        value={fieldValue}
-        onChange={(updatedDate: DateTime | null, keyboardInputValue?: string | undefined) => {
-          console.log('>>>> onChange', updatedDate?.isValid, keyboardInputValue);
+        value={inputValue}
+        onChange={(dateTime: DateTime | null, keyboardInputValue?: string | undefined) => {
+          if (!dateTime) return setInputValue(dateTime);
 
-          const updatedValue = updatedDate?.isValid ? fromDateTime(updatedDate, valueFormat) : keyboardInputValue;
+          if (formikProps?.setFieldValue) {
+            if (!dateTime.isValid) {
+              formikProps.setFieldValue(props.name, keyboardInputValue);
+              return setInputValue(dateTime);
+            } else {
+              if (valueFormat) formikProps.setFieldValue(props.name, dateTime.toFormat(valueFormat));
+              else formikProps.setFieldValue(props.name, dateTime.toJSDate());
+            }
+          }
 
-          if (!updatedValue) return;
-
-          if (formikProps?.setFieldValue) formikProps.setFieldValue(props.name, updatedValue);
-          if (typeof onChange === 'function') onChange(updatedValue);
+          setInputValue(dateTime);
         }}
         inputFormat={inputFormat || valueFormat}
         disablePast={disablePast}
@@ -72,7 +67,6 @@ export const InputDate: FC<InputDateProps> = ({
               (formikProps?.errors && formikProps?.errors[props.name]) ||
               (formikProps?.values && formikProps?.values[props.name] === 'Invalid DateTime')
             }
-            formikProps={formikProps}
             className={classNames('lc-input-date', className)}
           />
         )}
