febff8dd84f04c5dd4417db36b55ac1a748e0c3c
Jake Ruesink <jaruesink@gmail.com>
2023-03-17 00:49:04 -0500

    fixing the date picker to start with sunday

--- packages/ui-components/src/lib/inputs/InputDate/InputDate.tsx
+++ packages/ui-components/src/lib/inputs/InputDate/InputDate.tsx
@@ -9,6 +9,38 @@ import { InputText } from '../InputText/InputText';
 import { InputProps } from '../InputBase';
 import { useEffect } from 'react';
 
+// https://github.com/mui/material-ui/issues/30591#issuecomment-1377997824
+class Adapter extends AdapterLuxon {
+  public getWeekdays = () => {
+    return ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+  };
+
+  getWeekArray = (date: DateTime) => {
+    const { days } = date.endOf('month').endOf('week').diff(date.startOf('month').startOf('week'), 'days').toObject();
+
+    let weeks: DateTime[][] = [];
+    new Array(Math.round(days ?? 0))
+      .fill(0)
+      .map((_, i) => i)
+      .map(day => date.startOf('month').startOf('week').minus({ days: 1 }).plus({ days: day }))
+      .forEach((v, i) => {
+        if (i === 0 || (i % 7 === 0 && i > 6)) {
+          weeks.push([v]);
+          return;
+        }
+
+        weeks[weeks.length - 1].push(v);
+      });
+
+    weeks = weeks.filter(week => {
+      // do not allow weeks with start or end outside of current month
+      return week[0].hasSame(date, 'month') || week[week.length - 1].hasSame(date, 'month');
+    });
+
+    return weeks;
+  };
+}
+
 export type InputDateProps = Omit<InputProps, 'onChange'> & {
   value?: Date | string;
   onChange?: (date: Date | string | null) => void;
@@ -44,7 +76,7 @@ export const InputDate: FC<InputDateProps> = ({
   }, [fieldValue]);
 
   return (
-    <LocalizationProvider dateAdapter={AdapterLuxon}>
+    <LocalizationProvider dateAdapter={Adapter}>
       <DatePicker
         {...datePickerProps}
         label={label}
